0e8bac0aafe913a4d0470f81d4962ba604dd3cf3
jyg1001 <158987979@qq.com>
2016-11-03 16:51:55 +0800

    Update ReducerOfIPFilter.java

--- ex4/ReducerOfIPFilter.java
+++ ex4/ReducerOfIPFilter.java
@@ -1,12 +1,19 @@
 package No4;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class ReducerOfIPFilter {
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
-		while (scanner.hasNext()){
+		HashSet<String>set=new HashSet<String>();//利用hashset去重
+		while(scanner.hasNext()){
 			String line = scanner.nextLine();
+			String str[]=line.split("\t");
+			if(str.length!=2)//判断切分格式是否正确
+				continue;
+			set.add(str[0]);//切分正确的话，将ip添加进入hashset
 		}
+		System.out.println(set.size());//因其自动去重，因此，set的size即为去重后的ip总数
 	}
 }
